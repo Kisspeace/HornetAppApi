2d4464c5bb5b87b8c9575399aeee8d5bab3bee41
kisspeace <kisspeace@github.com>
2022-08-27 21:44:43 -0400

    version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name = 'HornetAppApi', 
-    version = '0.1.1', 
+    name = 'HornetAppApi',
+    version = '1.0.0',
     author='Kisspeace',
     # author_email='Kisspeace@example.com',
     url='http://github.com/Kisspeace/HornetAppApi',
