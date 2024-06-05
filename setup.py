d3486f0fe176c0ebad7a68cc7f6321854e08deab
kisspeace <kisspeace@github.com>
2024-06-06 02:03:23 +0300

    Version: 1.1.0.

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 
 setup(
     name = 'HornetAppApi',
-    version = '1.0.0',
+    version = '1.1.0',
     author='Kisspeace',
-    # author_email='Kisspeace@example.com',
     url='http://github.com/Kisspeace/HornetAppApi',
     description='gethornet.com API wrapper',
     packages = ['HornetAppApi', 'HornetAppApi.aiohttp', 'HornetAppApi.requests'],
